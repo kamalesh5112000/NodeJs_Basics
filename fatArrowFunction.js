349fa95cd395379d6702eb0f1aa3c125b3821575
Kamalesh <kamalesh5112000@pec.edu.com>
2023-06-20 18:37:03 +0530

    second commit

---
+++ fatArrowFunction.js
@@ -0,0 +1,7 @@
+const add1=(num1,num2)=>{
+    return `Sum of ${num1} and ${num2} is ${num1+num2}`
+
+}
+const add2=(num1,num2)=> `Sum of ${num1} and ${num2} is ${num1+num2}`;
+console.log(add1(5,10))
+console.log(add2(2,10))
--- firstCode.js
+++
@@ -1,3 +0,0 @@
-console.log("Hello World");
-const fs = require('fs');
-fs.writeFileSync("Hello.txt","Hello World");
